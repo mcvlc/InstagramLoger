64753acb091177e49145336078b3fa1a154c5b07
Vladislav <darkhorsestd@mail.ru>
2017-09-18 14:44:06 +0200

    Initial commit

---
+++ app/src/main/java/com/project/darkhorsestd/instagramloger/InstaActivity.java
@@ -0,0 +1,13 @@
+package com.project.darkhorsestd.instagramloger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InstaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insta);
+    }
+}
