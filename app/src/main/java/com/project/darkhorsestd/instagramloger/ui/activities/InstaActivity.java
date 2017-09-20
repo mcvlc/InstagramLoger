812b348a43f59db249091f907462b8ef33967dc5
Vladislav <darkhorsestd@mail.ru>
2017-09-20 21:52:15 +0200

    huge amount of changes

--- app/src/main/java/com/project/darkhorsestd/instagramloger/ui/activities/InstaActivity.java
+++ app/src/main/java/com/project/darkhorsestd/instagramloger/ui/activities/InstaActivity.java
@@ -1,15 +1,106 @@
 package com.project.darkhorsestd.instagramloger.ui.activities;
 
+import android.content.Intent;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.instagram.instagramapi.activities.InstagramAuthActivity;
+import com.instagram.instagramapi.engine.InstagramEngine;
+import com.instagram.instagramapi.engine.InstagramKitConstants;
+import com.instagram.instagramapi.exceptions.InstagramException;
+import com.instagram.instagramapi.interfaces.InstagramLoginCallbackListener;
+import com.instagram.instagramapi.objects.IGSession;
+import com.instagram.instagramapi.utils.InstagramKitLoginScope;
+import com.instagram.instagramapi.widgets.InstagramLoginButton;
 import com.project.darkhorsestd.instagramloger.R;
 
 public class InstaActivity extends AppCompatActivity {
+
+    private Button mLoginButton;
+    private DrawerLayout mNavigationDrawer;
+    private NavigationView mNavigationView;
+    private CoordinatorLayout mCoordinatorLayout;
+
+    String[] scopes = {InstagramKitLoginScope.BASIC};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insta);
+
+        mNavigationDrawer = (DrawerLayout) findViewById(R.id.navigation_drawer);
+        mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
+        View header = mNavigationView.getHeaderView(0);
+        mLoginButton = (Button) header.findViewById(R.id.loginButton);
+        mLoginButton.setOnClickListener(loginOnClickListener);
+
+
     }
+
+
+    View.OnClickListener loginOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(InstaActivity.this, InstagramAuthActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |
+                    Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+            intent.putExtra(InstagramEngine.TYPE, InstagramEngine.TYPE_LOGIN);
+            intent.putExtra(InstagramEngine.SCOPE, scopes);
+
+            startActivityForResult(intent, InstagramEngine.REQUEST_CODE_LOGIN);
+        }
+    };
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case InstagramEngine.REQUEST_CODE_LOGIN:
+
+                if (resultCode == RESULT_OK) {
+
+                    Bundle bundle = data.getExtras();
+
+                    if (bundle.containsKey(InstagramKitConstants.kSessionKey)) {
+
+                        IGSession session = (IGSession) bundle.getSerializable(InstagramKitConstants.kSessionKey);
+
+                        Toast.makeText(InstaActivity.this, "Logged" + session.getAccessToken(),
+                                Toast.LENGTH_LONG).show();
+
+                    }
+                }
+                break;
+            case InstagramEngine.REQUEST_CODE_LOGOUT:
+                if (resultCode == RESULT_OK) {
+
+                    Toast.makeText(InstaActivity.this, "Logged Out Successfully.",
+                            Toast.LENGTH_LONG).show();
+                }
+            default:
+                break;
+        }
+
+    }
+
+    public void genericClickListener(View v){
+        switch(v.getId()){
+            case R.id.logout:
+                InstagramEngine.getInstance(InstaActivity.this).logout(InstaActivity.this, InstagramEngine.REQUEST_CODE_LOGOUT);
+        }
+    }
+
+
+
+
 }
---
+++ app/src/main/java/com/project/darkhorsestd/instagramloger/utils/FollowersCounter.java
@@ -0,0 +1,8 @@
+package com.project.darkhorsestd.instagramloger.utils;
+
+/**
+ * Created by Lenovo on 20.09.2017.
+ */
+
+public class FollowersCounter {
+}
